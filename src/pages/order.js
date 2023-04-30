210f2f075058637e7dea041d272eb122fa02a78b
Kowalski Dragon <kowalski7cc@users.noreply.github.com>
2023-04-30 23:37:38 +0200

    Remove warnings

--- src/pages/order.js
+++ src/pages/order.js
@@ -1,15 +1,11 @@
 import * as React from "react";
-import { graphql, navigate } from "gatsby";
+import { navigate } from "gatsby";
 import QRCode from "react-qr-code";
 
 import Layout from "../components/layout";
 import Button from "react-bootstrap/Button";
 
-const OrderPage = ({ data, location }) => {
-  const params = new URLSearchParams(
-    typeof window !== "undefined" && window.location.search
-  );
-  console.log(typeof window !== "undefined" && window.location.search);
+const OrderPage = ({ location }) => {
   return (
     <Layout className="mb-5" back={true} title="Ordine">
       <h1 className="mb-4">Grazie per aver ordinato!</h1>
@@ -22,7 +18,9 @@ const OrderPage = ({ data, location }) => {
           <small>Mostra il codice QR al bar per pagare il tuo ordine.</small>
         </p>
         <div className="d-grid gap-2 mt-4">
-          <Button onClick={() => navigate("/")}>Nuovo ordine</Button>
+          <Button onClick={() => navigate("/", { state: {} })}>
+            Nuovo ordine
+          </Button>
         </div>
       </div>
     </Layout>
