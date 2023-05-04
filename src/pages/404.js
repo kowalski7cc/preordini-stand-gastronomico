f1018ae4066eb80f09dd8421adfbc71e7c93572b
Kowalski Dragon <kowalski7cc@users.noreply.github.com>
2023-05-04 18:53:44 +0200

    Improve 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,17 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import Seo from "../components/seo";
 
-const ErrorPage = ({ data }) => {
+const ErrorPage = () => {
   return (
-    <Layout className="mb-5" back={false} title="Pagina non trovata"></Layout>
+    <Layout className="mb-5" back={false} title="Pagina inesistente">
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <h1 className="display-1">404</h1>
+        <h2 className="display-6">Pagina inesistente</h2>
+      </div>
+    </Layout>
   );
 };
 
 export default ErrorPage;
+export const Head = () => <Seo title="Pagina inesistente" />;
